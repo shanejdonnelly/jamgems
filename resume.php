2da7467fccaf067a35a0c922a4646419cd987e74
Shane Donnelly <shanejdonnelly@gmail.com>
2013-03-20 23:50:22 -0400

    move resume out of google doc iframe

--- resume.php
+++ resume.php
@@ -1,11 +1,73 @@
 <?php 
-  include('includes/functions.php'); 
-  do_header('Resume', '');
-  
-  include('includes/nav.php');
+include('includes/functions.php'); 
+do_header('Resume', '');
+
+include('includes/nav.php');
 ?>
 
     <div class="container">
- 
-<iframe height=1000 width=990 src="https://docs.google.com/document/d/1DScRIDK7DeGGPfXimyZrExJA3-KOYrw0CcmQ5LvBp_c/pub?embedded=true"></iframe>
+
+<!-- iframe height=1000 width=990 src="https://docs.google.com/document/d/1DScRIDK7DeGGPfXimyZrExJA3-KOYrw0CcmQ5LvBp_c/pub?embedded=true"></iframe -->
+<div class="row" style=" margin-bottom:24px;">
+  <div class="span8">
+    <h1 class="resume_title">Shane Donnelly</h1>
+  </div>
+  <div class="span4">
+    <address>Searsport, ME<br/>shanejdonnelly@gmail.com<br/>
+    <a href="http://shane3.com/portfolio" title="Shane Donnelly Portfolio">http://shane3.com/portfolio</a>
+    <br/>
+    <a href="http://linkedin.com/in/shanejdonnelly" title="Shane Donnelly on LinkedIn">http://linkedin.com/in/shanejdonnelly</a>
+    </address>
+  </div>
+</div>
+<div class="row">
+  <div class="span8 resume">
+    <h1 class="resume_header">Experience</h1>
+    <h3>Web Developer, Timberland</h3>
+    <h4>Stratham, NH &mdash; 2011-Present</h4>
+    <ul>
+      <li>Develop feature rich, mobile optimized websites utilizing the latest web technologies.</li>
+      <li>Update multinational e-commerce sites and code marketing emails</li>
+      <li>Collaborate with designers, project managers and clients.</li>
+    </ul>
+    <h3>Web Developer, Freelance</h3>
+    <h4>Searsport, ME &mdash; 2008-Present</h4>
+    <ul>
+      <li>Code front and back end of projects</li>
+      <li>Administer LAMP server</li>
+      <li>Communicate with clients to establish project parameters</li>
+    </ul>
+    <h3>Owner, Belfast Music</h3>
+    <h4>Belfast, ME &mdash; 2004-2008</h4>
+    <ul>
+      <li>Built a retail music store from the ground up</li>
+      <li>Managed all daily operations</li>
+    </ul>
+  </div>
+  <div class="span4">
+    <h1 class="resume_header">Skills</h1>
+    <ul>
+      <li>HTML5/CSS3</li>
+      <li>Javascript/jQuery</li>
+      <li>Linux/Apache/MySQL/PHP</li>
+      <li>Wordpress</li>
+      <li>Photoshop</li>
+      <li>Git</li>
+      <li>Vim</li>
+    </ul>
+    <h1 class="resume_header">Portfolio</h1>
+    <ul>
+      <li><a href="http://lookbook.timberland.com/en/2013/fall/tree/pr" title="Timberland Lookbook">http://lookbook.timberland.com</a></li>
+      <li><a href="http://timberlandpro.com" title="Timberland PRO">http://timberlandpro.com</a></li>
+      <li><a href="http://timberlandbootcompany.com" title="Timberland Boot Company">http://timberlandbootcompany.com</a></li>
+      <li><a href="../portfolio" title="Shane Donnelly Portfolio">Full Portfolio</a></li>
+    </ul>
+    <h1 class="resume_header">Education</h1>
+    <ul class="unstyled">
+      <li>BS, Information Technology, 2012</li>
+      <li>University of Massachusetts, Lowell</li>
+    </ul>
+  </div>
+</div>
 <?php include('includes/footer.php'); ?>
+
