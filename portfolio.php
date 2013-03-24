fe5220471100dc4bc5bd6436f971098ccc724bd5
Shane Donnelly <shanejdonnelly@gmail.com>
2013-03-24 15:54:05 -0400

    update portfolio

--- portfolio.php
+++ portfolio.php
@@ -16,10 +16,8 @@
         </div>
         <div class="span2">
           <h3 class="code">Code Samples</h3>
-          <p class="button_wrap"><a class="btn " target="_blank" href="https://github.com/shanejdonnelly/portfolio/blob/master/timberland_lookbook/script.js">JS &raquo;</a></p>
-          <p class="button_wrap"><a class="btn " target="_blank" href="https://github.com/shanejdonnelly/portfolio/blob/master/timberland_lookbook/plugins.js">JS &raquo;</a></p>
-          <p class="button_wrap"><a class="btn " target="_blank" href="https://github.com/shanejdonnelly/portfolio/blob/master/timberland_lookbook/style.css">CSS &raquo;</a></p>
-        </div>
+          <p class="button_wrap"><a class="btn " target="_blank" href="https://github.com/shanejdonnelly/portfolio/tree/master/timberland_lookbook">JS &raquo;</a></p>
+</div>
         <div class="span5">
           <h2>Timberland Lookbook<sup>&reg;</sup></h2>
           <hr>
@@ -41,66 +39,14 @@
             house the actual image src and a placeholder image until the image is near the viewport, lazyload was perfect for this application.            
           </p>
           <p>
-            One of my favorite bits of this site was coding the custom animations that are scattered throughout. The site was designed to be 
-            interactive and has lots of panning and movement. The navigation bar, while subtle, required a lot of thought to get the detail right. 
-            A few noteworthy features: <br/>
-            <ul>
-              <li>The size of the nav bar is automatically scaled to the size of the overall page</li>
-              <li>The indicator is proportionally scaled to represent the user's viewport size</li>
-            </ul>
-          </p>
-          <p>
             I didn't use a javascript framework for this site, instead relying 
             on a few namespace chunks and jQuery plugins where appropriate to keep the code lean and organized. 
           </p>
         </div>
       </div><!-- .row -->
-      
-      <hr>
-      
-      <!-- Timberland BootCo -->
-      <div class="row portfolio">
-        <div class="span4">
-          <a class="portfolio_image" href="http://timberlandbootcompany.com" title="Visit Timberland Boot Company" target="_blank"><img src="images/tbl_bootco_400.png" alt="Timberland Boot Company" /></a>
-        </div>
-        <div class="span2">
-          <h3 class="code">Code Samples</h3>
-          <p class="button_wrap"><a class="btn " target="_blank" href="https://github.com/shanejdonnelly/portfolio/blob/master/timberland_bootco/functions.php">PHP &raquo;</a></p>
-          <p class="button_wrap"><a class="btn " target="_blank" href="https://github.com/shanejdonnelly/portfolio/blob/master/timberland_bootco/template.php">PHP &raquo;</a></p>
-          <p class="button_wrap"><a class="btn " target="_blank" href="https://github.com/shanejdonnelly/portfolio/blob/master/timberland_bootco/lang_en.xml">XML &raquo;</a></p>
-          <p class="button_wrap"><a class="btn " target="_blank" href="https://github.com/shanejdonnelly/portfolio/blob/master/timberland_bootco/plugins.js">JS &raquo;</a></p>
-          <p class="button_wrap"><a class="btn " target="_blank" href="https://github.com/shanejdonnelly/portfolio/blob/master/timberland_bootco/style.css">CSS &raquo;</a></p>
-          
-        </div>
-        <div class="span5">
-          <h2>Timberland Boot Company<sup>&reg;</sup></h2>
-          <hr>
-          <h3>Technologies:</h3>
-          <div class="row">
-            <p class="span1">PHP</p>
-            <p class="span1">XML</p>
-            <p class="span1">jQuery</p>
-          </div><!-- .row -->
-          <div class="row">
-            <p class="span1">HTML5</p>
-            <p class="span1">CSS3</p>
 
-          </div><!-- .row --> 
-          <h3>About:</h3>      
-          <p>
-            A fairly simple site in many respects, Timberland Boot Company still presented some challenges. Chief among these was 
-            the fact that the site needed to be multi-lingual. In this instance, the solution was to use xml files to house translations. 
-            The xml was parsed in the PHP and output based on the user's language setting.
-          </p>   
-          <p>
-            To allow for the layouts of the various pages to change easily, I coded a number of PHP functions to output modular chunks 
-            of html. I also coded a few custom jQuery scripts. 
-          </p>
-        </div>
-      </div><!-- .row -->
-      
       <hr>
-      
+
       <!-- Timberland PRO -->
       <div class="row portfolio">
         <div class="span4">
@@ -138,43 +84,50 @@
       </div><!-- .row -->            
 
       <hr>
-      
-      <!-- Koch Brothers -->
+
+      <!-- Timberland BootCo -->
       <div class="row portfolio">
         <div class="span4">
-          <a class="portfolio_image" href="http://www.kochbrothers.democracyforamerica.com/" title="Visit Koch Brothers Movie Night" target="_blank"><img src="images/koch_400.jpg" alt="Koch Brothers Movie Night" /></a>
+          <a class="portfolio_image" href="http://timberlandbootcompany.com" title="Visit Timberland Boot Company" target="_blank"><img src="images/tbl_bootco_400.png" alt="Timberland Boot Company" /></a>
         </div>
         <div class="span2">
-          <h3 class="code">&nbsp;</h3>
-          
+          <h3 class="code">Code Samples</h3>
+          <p class="button_wrap"><a class="btn " target="_blank" href="https://github.com/shanejdonnelly/portfolio/blob/master/timberland_bootco/functions.php">PHP &raquo;</a></p>
+          <p class="button_wrap"><a class="btn " target="_blank" href="https://github.com/shanejdonnelly/portfolio/blob/master/timberland_bootco/template.php">PHP &raquo;</a></p>
+          <p class="button_wrap"><a class="btn " target="_blank" href="https://github.com/shanejdonnelly/portfolio/blob/master/timberland_bootco/lang_en.xml">XML &raquo;</a></p>
+          <p class="button_wrap"><a class="btn " target="_blank" href="https://github.com/shanejdonnelly/portfolio/blob/master/timberland_bootco/plugins.js">JS &raquo;</a></p>
+      
+
         </div>
         <div class="span5">
-          <h2>Koch Brothers Movie Night</h2>
+          <h2>Timberland Boot Company<sup>&reg;</sup></h2>
           <hr>
-          <h3>Technologies Used:</h3>
+          <h3>Technologies:</h3>
           <div class="row">
-            <p class="span1">SASS</p>
-            <p class="span1">Bootstrap</p>
-            <p class="span1">Git</p>
-          </div><!-- .row -->
-          <div class="row">
-            <p class="span1">Rails</p>
+            <p class="span1">PHP</p>
+            <p class="span1">XML</p>
             <p class="span1">jQuery</p>
-            <p class="span1">HTML5</p>
           </div><!-- .row -->
-          <h3>About:</h3>        
+          <div class="row">
+            <p class="span1">HTML5</p>
+            <p class="span1">CSS3</p>
+
+          </div><!-- .row --> 
+          <h3>About:</h3>      
           <p>
-            This was my first use of SASS, Ruby on Rails, Bootstrap and Git - oh my! Built in a very short timeframe with a pair of 
-            Rails ninjas (who handled the heavy lifting in that department), it was a lot of fun.
+            A fairly simple site in many respects, Timberland Boot Company still presented some challenges. Chief among these was 
+            the fact that the site needed to be multi-lingual. In this instance, the solution was to use xml files to house translations. 
+            The xml was parsed in the PHP and output based on the user's language setting.
           </p>   
           <p>
-            I handled all of the front end development and also came up with the simple design. Unfortunately, it was a short lived 
-            project and I wasn't able to hang on to any code samples.
+            To allow for the layouts of the various pages to change easily, I coded a number of PHP functions to output modular chunks 
+            of html. I also coded a few custom jQuery scripts. 
           </p>
         </div>
       </div><!-- .row -->
 
-      <hr>      
+      <hr>
+
       <!-- Timberland Tech -->
       <div class="row portfolio">
         <div class="span4">
@@ -214,7 +167,43 @@
 
 
       <hr>
-      
+
+      <!-- Koch Brothers -->
+      <div class="row portfolio">
+        <div class="span4">
+          <a class="portfolio_image" href="http://www.kochbrothers.democracyforamerica.com/" title="Visit Koch Brothers Movie Night" target="_blank"><img src="images/koch_400.jpg" alt="Koch Brothers Movie Night" /></a>
+        </div>
+        <div class="span2">
+          <h3 class="code">&nbsp;</h3>
+
+        </div>
+        <div class="span5">
+          <h2>Koch Brothers Movie Night</h2>
+          <hr>
+          <h3>Technologies Used:</h3>
+          <div class="row">
+            <p class="span1">SASS</p>
+            <p class="span1">Bootstrap</p>
+            <p class="span1">Git</p>
+          </div><!-- .row -->
+          <div class="row">
+            <p class="span1">Rails</p>
+            <p class="span1">jQuery</p>
+            <p class="span1">HTML5</p>
+          </div><!-- .row -->
+          <h3>About:</h3>        
+          <p>
+            This was my first use of SASS, Ruby on Rails, Bootstrap and Git - oh my! Built in a very short timeframe with a pair of 
+            Rails ninjas (who handled the heavy lifting in that department), it was a lot of fun.
+          </p>   
+          <p>
+            I handled all of the front end development and also came up with the simple design. Unfortunately, it was a short lived 
+            project and I wasn't able to hang on to any code samples.
+          </p>
+        </div>
+      </div><!-- .row -->
+
+      <hr>      
       <!-- Loon Echo -->
       <div class="row portfolio">
         <div class="span4">
@@ -240,7 +229,7 @@
           <h3>About:</h3>        
           <p>
             One of the requirements for this site was to minimize the use of images. This was quite a challenge given the 
-            design of the site, but I enjoyed fully utilizing CSS3 to create some neat shapes and effects. I also created a few custom 
+            design of the site, but I enjoyed utilizing CSS to create some neat shapes and effects. I also created a few custom 
             jQuery scripts for the site.
           </p>   
           <p>
@@ -249,31 +238,6 @@
         </div>
       </div><!-- .row -->
    <hr>
-
-      <!-- Shell Scripts -->
-      <div class="row portfolio">
-        <div class="span4">
-          &nbsp;
-        </div>
-        <div class="span2">
-          <h3 class="code">Code Samples</h3>
-          <p class="button_wrap"><a class="btn" target="_blank" href="https://github.com/shanejdonnelly/portfolio/blob/master/shell_scripts/change-lines">SH &raquo;</a></p>
-          <p class="button_wrap"><a class="btn" target="_blank" href="https://github.com/shanejdonnelly/portfolio/blob/master/shell_scripts/clean_list">SH &raquo;</a></p>
-          <p class="button_wrap"><a class="btn" target="_blank" href="https://github.com/shanejdonnelly/portfolio/blob/master/shell_scripts/mywhich">SH &raquo;</a></p>
-          <p class="button_wrap"><a class="btn" target="_blank" href="https://github.com/shanejdonnelly/portfolio/blob/master/shell_scripts/s_mailer">SH &raquo;</a></p>
-        </div>
-        <div class="span5">
-          <h2>Shell Scripts</h2>
-          <hr>  
-          <h3>About:</h3>        
-          <p>I recently took a class in shell scripting. I'll never be a system admin (nor do I aspire to be),
-           but I learned a good number of tricks from the class. I've
-            included these code samples here mainly as an illustration of my ability to 
-            work from the command line.</p>   
-        </div>
-      </div><!-- .row -->
-
-      <hr>
 
    <!--
       <p class="button_wrap" style="float:right;"><a class="btn btn-large btn-primary" href="portfolio2.php">More &raquo;</a></p>
--- portfolio2.php
+++ portfolio2.php
@@ -9,6 +9,31 @@
       
       <!-- Portfolio -->
       
+
+      <!-- Shell Scripts -->
+      <div class="row portfolio">
+        <div class="span4">
+          &nbsp;
+        </div>
+        <div class="span2">
+          <h3 class="code">Code Samples</h3>
+          <p class="button_wrap"><a class="btn" target="_blank" href="https://github.com/shanejdonnelly/portfolio/blob/master/shell_scripts/change-lines">SH &raquo;</a></p>
+          <p class="button_wrap"><a class="btn" target="_blank" href="https://github.com/shanejdonnelly/portfolio/blob/master/shell_scripts/clean_list">SH &raquo;</a></p>
+          <p class="button_wrap"><a class="btn" target="_blank" href="https://github.com/shanejdonnelly/portfolio/blob/master/shell_scripts/mywhich">SH &raquo;</a></p>
+          <p class="button_wrap"><a class="btn" target="_blank" href="https://github.com/shanejdonnelly/portfolio/blob/master/shell_scripts/s_mailer">SH &raquo;</a></p>
+        </div>
+        <div class="span5">
+          <h2>Shell Scripts</h2>
+          <hr>  
+          <h3>About:</h3>        
+          <p>I recently took a class in shell scripting. I'll never be a system admin (nor do I aspire to be),
+           but I learned a good number of tricks from the class. I've
+            included these code samples here mainly as an illustration of my ability to 
+            work from the command line.</p>   
+        </div>
+      </div><!-- .row -->
+
+      <hr>
       <!-- Timberland BootCo -->
       <div class="row portfolio">
         <div class="span4">
